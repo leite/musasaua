eac4efc373db66354b2b0f857ef023cd9b28d078
leite <xxleite@gmail.com>
2013-06-15 13:51:23 -0300

    luvit package style

---
+++ package.lua
@@ -0,0 +1,7 @@
+return {
+  name        = "musasaua",
+  author      = "xxleite <xxleite@gmail.com>",
+  description = "musasaua is a http/https library with built in support for proxy, cookies and redirect, written in pure Lua on top of Luvit.",
+  version     = "0.0.1",
+  main        = "musasaua.lua"
+}
